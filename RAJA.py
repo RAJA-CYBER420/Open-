b55e2b481ebfc9c910d3f4a5203a9fde61f0715f
RAJA-CYBER420 <ntayyab489@gmail.com>
2026-07-30 14:33:21 +0500

    Update GitHub link in RAJA.py

--- RAJA.py
+++ RAJA.py
@@ -50,7 +50,7 @@ def raja_approval():
     
     # Yahan apni GitHub link lagayein jahan approved keys hongi
     # Format: Raw text file on GitHub
-    github_link = "https://github.com/RAJA-CYBER420/RAJA_OK/blob/main/APROVEL-78"
+    github_link = "https://github.com/RAJA-CYBER420/Open-/blob/main/aprovel-73"
     
     print('''
 \033[1;31m ██████╗  █████╗      ██╗ █████\x1b[0m╗ 
